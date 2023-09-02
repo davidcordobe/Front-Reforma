d759316c92085f9e5d82be1e3c19c924a3e64303
David Arce <arce475@gmail.com>
2023-09-02 10:30:26 -0300

    quienSomos

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import '../css/home.css';
-import HomeImg from '../components/homeImg';
+
 
 const Home = () => {
     return (
         <div className="home-container container fade-enter-active">
             <h2 className="titulo-home">REFORMA 2.7</h2>
             <div className="intro-container">
-                <HomeImg className="imagen-tu-foto" />
                 <p className="descripcion-home">
-                    Bienvenidos a REFORMA 2.7, una marca hecha a mano que se destaca por la renovación, reutilización y remodelación de muebles y objetos, creando espacios que puedan conectar con el usuario. Permíteme contarte un poco sobre nosotros. Somos Ade y Nina, madre e hija, trabajando en un proyecto de diseños de interiores que recupera muebles, objetos y espacios en su estado actual para reformarlos y reinventarnos. Creemos que estos objetos tienen una historia que contar, por eso nos encanta recuperarlos y extender su vida útil.
-                    Nos especializamos en crear espacios únicos y funcionales que se adapten a las necesidades de los clientes. Mantenemos un fuerte compromiso con el medio ambiente y la sostenibilidad, y creemos que cada mueble merece una segunda oportunidad. Por eso, con Reforma 2.7, transformamos muebles viejos, desgastados o en desuso en piezas únicas y personalizadas, dándoles una nueva vida. A menudo, estos muebles se descartan porque están dañados, pasados de moda o simplemente no encajan con la estética actual de una residencia, pero para nosotros, son un tesoro esperando ser descubierto.
-                    ¡Es un placer para nosotros que formes parte de nuestro emprendimiento!
+                    Proximamente algo Nuevo que te sorprendera!!!
                 </p>
             </div>
         </div>
